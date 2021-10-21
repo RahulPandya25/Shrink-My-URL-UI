787a1006bbd3da0046ebcb3218fa390ee6c240ba
Rahul <rahul25796@gmail.com>
2021-10-21 00:18:43 -0400

    adds Main component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,37 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MainComponent } from './Components/main/main.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+    // default route 1
+    { path: "", component: MainComponent},
+    // default route 2
+    // { path: "", redirectTo: "/tutor/subjects", pathMatch: "full" },
+  
+    // // tutor
+    // { path: "tutor", component: TutorSubjectsComponent },
+    // { path: "tutor/createSubject", component: CreateSubjectComponent },
+    // { path: "tutor/tests", component: TutorTestsComponent },
+    // { path: "tutor/tests/uploadTest", component: UploadTestComponent },
+    // { path: "tutor/tests/studentList", component: StudentListComponent },
+  
+    // // student
+    // { path: "student", component: StudentSubjectsComponent },
+    // { path: "student/tests", component: StudentTestsComponent },
+    // { path: "student/tests/takeTest", component: TakeTestComponent },
+  
+    // // misc
+    // { path: "about", component: AboutComponent },
+    // { path: "report", component: ReportComponent },
+  
+    // // unauthorize error page
+    // { path: "unauthorized", component: UnauthorizedComponent },
+  
+    // // page not found route ALWAYS at last
+    // { path: "**", component: PageNotFoundComponent }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
