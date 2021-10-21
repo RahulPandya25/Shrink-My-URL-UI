2be9dce0dcf1bd28242ca58b599af0bab2261cd0
Rahul <rahul25796@gmail.com>
2021-10-21 00:31:14 -0400

    adds express server

---
+++ bin/server.conf.js
@@ -0,0 +1,40 @@
+// ******************** //
+// server configuration //
+// ******************** //
+
+var express = require("express");
+var cors = require("cors");
+var path = require("path");
+
+var app = express();
+
+const ANGULAR_BUNDLE = "dist";
+
+app.use(
+  cors({
+    origin: "http://localhost:4200",
+  })
+);
+
+//make angular dist folder available for public
+app.use(express.static(path.join(__dirname, ANGULAR_BUNDLE)));
+
+// all the other request should be redirected to angular app
+app.use("*", express.static(path.join(__dirname, ANGULAR_BUNDLE)));
+
+
+var debug = require("debug")("mean-app:server");
+var http = require("http");
+
+var port = process.env.PORT || "4300";
+app.set("port", port);
+
+var server = http.createServer(app);
+server.listen(port);
+server.on("listening", onListening);
+
+function onListening() {
+  var addr = server.address();
+  debug("Listening on " + port);
+  console.log("Listening on " + port);
+}
